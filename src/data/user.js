d27eeec24cb4dce2ba2f487aa062b6e3a655a591
Ian <ian.mwangi1@student.moringaschool.com>
2024-04-29 12:08:05 +0300

    Complete lab

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Ian",
+  city: "Nairobi",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/IanMwangiii",
+    linkedin: "https://www.linkedin.com/in/ian-mwangi-7a4394306/?lipi=urn%3Ali%3Apage%3Afeed_index_index%3B822abefb-6040-4474-b6a8-a5611744e8e9",
   },
 };
 
